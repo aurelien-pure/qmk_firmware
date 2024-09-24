6fc35aac9fdba20861fe4bb2b2eeb3873e2378c4
Aurélien Kochalski <aurelien@pure-illusion.com>
2024-09-24 14:03:32 +0200

    Add french key codes + Lorem macro + PW macro

--- keyboards/keychron/q3/iso_encoder/keymaps/doraemon/keymap.c
+++ keyboards/keychron/q3/iso_encoder/keymaps/doraemon/keymap.c
@@ -15,6 +15,9 @@
  */
 
 #include QMK_KEYBOARD_H
+#include "keymap_french.h"
+
+/* #region [LAYERS] */
 
 enum layers{
     MAC_BASE,
@@ -26,34 +29,34 @@ enum layers{
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_tkl_f13_iso(
         KC_ESC,   KC_BRID,  KC_BRIU,  KC_NO,    KC_NO,    RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    KC_MUTE,  KC_NO,    KC_NO,    RGB_MOD,
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,              KC_DEL,   KC_END,   KC_PGDN,
-        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,    KC_ENT,
-        KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,            KC_UP,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     FR_RPRN,  FR_EQL,     KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
+        KC_TAB,   FR_A,     FR_Z,     FR_E,     FR_R,     FR_T,     FR_Y,     FR_U,     FR_I,     FR_O,     FR_P,     FR_CIRC,  FR_DLR,     KC_DEL,   KC_END,   KC_PGDN,
+        KC_CAPS,  FR_Q,     FR_S,     FR_D,     FR_F,     FR_G,     FR_H,     FR_J,     FR_K,     FR_L,     FR_M,     FR_UGRV,  FR_ASTR,    KC_ENT,
+        KC_LSFT,  KC_NUBS,  FR_W,     FR_X,     FR_C,     FR_V,     FR_B,     FR_N,     FR_COMM,  FR_SCLN,  FR_COLN,  FR_EXLM,              KC_RSFT,            KC_UP,
         KC_LCTL,  KC_LOPT,  KC_LCMD,                                KC_SPC,                                 KC_RCMD,  KC_ROPT,  MO(MAC_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [MAC_FN] = LAYOUT_tkl_f13_iso(
         _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     RGB_TOG,  _______,  _______,  RGB_TOG,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,
         RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              _______,  _______,  _______,
-        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,            _______,
+        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  M_LOREM,  _______,  _______,  _______,    M_PW,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              _______,            _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,  _______,  _______,  _______),
 
     [WIN_BASE] = LAYOUT_tkl_f13_iso(
         KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     KC_MUTE,  KC_PSCR,  KC_NO,    RGB_MOD,
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,              KC_DEL,   KC_END,   KC_PGDN,
-        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,    KC_ENT,
-        KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,            KC_UP,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     FR_RPRN,  FR_EQL,     KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
+        KC_TAB,   FR_A,     FR_Z,     FR_E,     FR_R,     FR_T,     FR_Y,     FR_U,     FR_I,     FR_O,     FR_P,     FR_CIRC,  FR_DLR,               KC_DEL,   KC_END,   KC_PGDN,
+        KC_CAPS,  FR_Q,     FR_S,     FR_D,     FR_F,     FR_G,     FR_H,     FR_J,     FR_K,     FR_L,     FR_M,     FR_UGRV,  FR_ASTR,    KC_ENT,
+        KC_LSFT,  KC_NUBS,  FR_W,     FR_X,     FR_C,     FR_V,     FR_B,     FR_N,     FR_COMM,  FR_SCLN,  FR_COLN,  FR_EXLM,              KC_RSFT,            KC_UP,
         KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [WIN_FN] = LAYOUT_tkl_f13_iso(
         _______,  KC_BRID,  KC_BRIU,  _______,  _______,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    RGB_TOG,  _______,  _______,  RGB_TOG,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,
         RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              _______,  _______,  _______,
-        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,            _______,
+        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  M_LOREM,  _______,  _______,  _______,    M_PW,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              _______,            _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,  _______,  _______,  _______),
 };
 
@@ -65,3 +68,50 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [WIN_FN]   = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI) }
 };
 #endif
+
+/* #endregion */
+
+/* #region [CUSTOM KEYCODES] */
+
+enum custom_keycodes {
+    M_LOREM = SAFE_RANGE,
+    M_PW,
+};
+
+/* #endregion */
+
+/* #region [CONSTANTS] */
+
+const char *const PW = "pure0sssn13\n";
+const char *const LOREMS[] = {
+    "Suspendisse magna ut fermentum tristique nec libero. ",
+    "Quisque vel tristique arcu. Suspendisse nec potenti. ",
+    "Integer luctus justo ac magna dignissim, id egestas. ",
+    "Lorem ipsum dolor sit amet, ligula nec justo tempor. ",
+    "Integer luctus justo ac magna dignissim, id egestas. "
+};
+#define LOREMS_LENGTH (sizeof(LOREMS) / sizeof(LOREMS[0])) // Compute array length
+
+/* #endregion */
+
+/* #region [KEYPRESS] */
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case M_LOREM:
+            if (record->event.pressed) {
+                int randomIndex = rand() % LOREMS_LENGTH; // Get a random index
+                send_string(LOREMS[randomIndex]); // Send the string
+            }
+            return false;
+        case M_PW:
+            if (record->event.pressed) {
+                send_string(PW);
+            }
+            return false;
+    }
+
+    return true;
+}
+
+/* #endregion */
